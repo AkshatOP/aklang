84dcca5ff12b12bcc4a9740c2fe38971e82eb36f
AkshatOP <hunterdevil0987@gmail.com>
2025-05-04 17:34:10 +0530

    website changes

--- website/src/utils/prism-aklang.js
+++ website/src/utils/prism-aklang.js
@@ -1,6 +1,16 @@
 import Prism from 'prismjs';
 
 Prism.languages.aklang = {
+  comment: [
+    {
+      pattern: /\/\/.*/m,          // single-line: // comment
+      greedy: true
+    },
+    {
+      pattern: /\/\*[\s\S]*?\*\//, // multi-line: /* comment */
+      greedy: true
+    }
+  ],
   keyword: /\b(?:class chalu|class khatam|note kar|bol|proxy mili toh|nahi mili toh|pakde gaye|jab tak|bunk maar|agli class|pass|fail)\b/,
   number: /\b\d+(?:\.\d+)?\b/,
   string: /(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,
